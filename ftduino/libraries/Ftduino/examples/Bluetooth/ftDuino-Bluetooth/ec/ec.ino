03ad683d821a43a92032e375cb4b256ae05d85c3
Till Harbaum <till@harbaum.org>
2021-01-27 14:45:13 +0100

    Add ec sketch

---
+++ ftduino/libraries/Ftduino/examples/Bluetooth/ftDuino-Bluetooth/ec/ec.ino
@@ -0,0 +1,46 @@
+#include <Wire.h>
+#include "I2cSerialBt.h"
+I2cSerialBt btSerial;
+
+void setup() {
+  Serial.begin(115200);
+//  while(!Serial);
+  
+  Wire.setClock(400000);
+  // wait max 1 sec for adapter to appear on bus. This is not
+  // needed as begin() will wait for the device. But this way
+  // we can use the led as an inidictaor for problems with 
+  // the i2c uart adapater
+  if(!btSerial.check(1000)) {
+    // fast blink with led on failure
+    pinMode(LED_BUILTIN, OUTPUT); 
+    while(true) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+    }
+  }
+  
+  btSerial.begin(9600);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  if(btSerial.available()) {
+    char chr = btSerial.read();
+    Serial.print(chr, HEX);
+    Serial.print(" ");
+    Serial.println(String(chr));
+    switch(chr) {
+      case 'r':
+        digitalWrite(LED_BUILTIN, HIGH); 
+        break;
+      
+      case 'l':
+        digitalWrite(LED_BUILTIN, LOW); 
+        break;
+    }
+  }
+}
